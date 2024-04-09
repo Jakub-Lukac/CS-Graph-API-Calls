b99f6ea744a41cce1a0e2528bc9a69837a0d81eb
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-04-09 11:52:32 +0100

    graph client initialized

---
+++ Graph-API-Calls-Practise/GraphClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Azure.Identity;
+
+namespace Graph_API_Calls_Practise
+{
+    internal class GraphClient
+    {
+        private GraphServiceClient _graphServiceClient;
+
+        public GraphClient(string tenantId, string clientId, string clientSecret)
+        {
+            _graphServiceClient = CreateGraphClient(tenantId, clientId, clientSecret);  
+        }
+
+        private GraphServiceClient CreateGraphClient(string tenantId, string clientId, string clientSecret)
+        {
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+            var options = new ClientSecretCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+            };
+
+            var clientSecretCredentials = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+            return new GraphServiceClient(clientSecretCredentials, scopes);
+        }
+    }
+}
---
+++ Graph-API-Calls-Practise/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Graph_API_Calls_Practise
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GraphClient graphClient = new GraphClient(Credentials.TENANT_ID, Credentials.CLIENT_ID, Credentials.CLIENT_SECRET);
+        }
+    }
+}
